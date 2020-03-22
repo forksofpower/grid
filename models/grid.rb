bcd36b12cf7410179c937cc27ae6f265e6e6580f
github username <github email address>
2020-03-22 01:19:24 -0500

    add linear_distance method to grid

--- models/grid.rb
+++ models/grid.rb
@@ -32,6 +32,33 @@ class Grid
   end
 end
 
+  # calculate the distance between two tiles
+  def linear_distance(tile_a, tile_b)
+    x1, y1 = tile_a.x, tile_a.y
+    x2, y2 = tile_b.x, tile_b.y
+    Math.sqrt((x2 - x1)**2 + (y2 - y1)**2).abs
+  end
 
+  # find a 90 degree path between two tiles
+  def find_path(tile_a, tile_b)
+    if rand(0..1).floor() === 1
+      # start horizontal
+      x, y = tile_a.x, tile_b.y
+    else
+      # start vertical
+      x, y = tile_b.x, tile_a.y
+    end
 
+    turn_point = tile_by_coordinates(x, y)
 
+    [tile_a, turn_point, tile_b]
+
+    # if (x2 > x1)
+    #   delta_x = x2 - x1
+    #   delta_y = y2 - y1
+    # else
+    #   delta_x = x1 - x2
+    #   delta_y = y1 - y2
+    # end
+  end
+end
